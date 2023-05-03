43a52920337530333956e6f66ad75348108e4eee
edenilsonpineda <edenilsonpineda@outlook.com>
2023-05-02 23:11:19 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# 👋 ChatGPT-Prompting for Developers
+
+This project uses OpenAI's API to perform natural language processing tasks. 
+
+## 🚀 Prerequisites
+
+Before you can run the project, you will need the following:
+
+- Python 3.6 or higher
+- An OpenAI API key
+
+## 🛠️ Setup
+
+To set up the project, follow these steps:
+
+1. Clone the repository to your local machine.
+2. Navigate to the project directory.
+3. Create a virtual environment using the following command:
+
+```bash
+python3 -m venv env
+```
+
+4. Activate the virtual environment using the following command:
+
+```bash
+source env/bin/activate
+```
+
+5. Install the required dependencies using the following command:
+
+```bash
+pip install -r requirements.txt
+```
+
+6. Create a new file named `.env` in the root directory of the project.
+7. Add your OpenAI API key to the `.env` file using the following format:
+
+```bash
+OPENAI_API_KEY=<your-api-key>
+```
+
+## 💻 Running the Project
+
+To run the project, make sure that the virtual environment is activated and the required dependencies are installed. Then, run the following command:
+
+```bash
+python prompt.py
+```
+
+The program will prompt you for input and use the OpenAI API to perform natural language processing tasks. 
+
+## 🛑 Deactivating the Virtual Environment
+
+To deactivate the virtual environment, simply run the following command:
+
+```bash
+deactivate
+```
+
+## 🎉 Conclusion
+
+Thank you for using this Python project! If you have any questions or feedback, please feel free to reach out to the project author.
---
+++ prompt.py
@@ -0,0 +1,168 @@
+import openai
+import os
+
+# @author: Edenilson Pineda
+# Comment the invocation of `get_completion` function to avoid consuming the API quota
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()); # reads local .env file
+
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0, # this is the degree of randomness of the model's output
+    )
+    return response.choices[0].message["content"]
+
+'''
+@Principle 1
+
+Tactic #1: Use delimiters to clearly indicate distinct parts of the input
+Delimeters can be anything like: ```, """, <>, <tag> </tag>, :
+'''
+
+text = f"""
+You should express what you want a model to do by \ 
+providing instructions that are as clear and \ 
+specific as you can possibly make them. \ 
+This will guide the model towards the desired output, \ 
+and reduce the chances of receiving irrelevant \ 
+or incorrect responses. Don't confuse writing a \ 
+clear prompt with writing a short prompt. \ 
+In many cases, longer prompts provide more clarity \ 
+and context for the model, which can lead to \ 
+more detailed and relevant outputs.
+"""
+prompt = f"""
+Summarize the text delimited by triple backticks \ 
+into a single sentence.
+```{text}```
+"""
+#response = get_completion(prompt)
+#print(response)
+
+
+
+####################
+
+'''
+@Principle 1
+Tactic #2: Ask for a structured output
+'''
+
+prompt = f"""
+Genera una lista de tres libros ficticios con su título, \
+autor, género y precio. Conviertelos en un formato JSON con los siguientes identificadores: \
+book_id, title, author, genre, price.
+"""
+#response = get_completion(prompt)
+#print(response)
+
+
+
+####################
+'''
+@Principle 1
+Tactic #3: Ask the model to check whether conditions are satisfied
+'''
+
+text_2 = f"""
+The sun is shining brightly today, and the birds are \
+singing. It's a beautiful day to go for a \ 
+walk in the park. The flowers are blooming, and the \ 
+trees are swaying gently in the breeze. People \ 
+are out and about, enjoying the lovely weather. \ 
+Some are having picnics, while others are playing \ 
+games or simply relaxing on the grass. It's a \ 
+perfect day to spend time outdoors and appreciate the \ 
+beauty of nature.
+"""
+prompt = f"""
+You will be provided with text delimited by triple quotes. 
+If it contains a sequence of instructions, \ 
+re-write those instructions in the following format:
+
+Step 1 - ...
+Step 2 - …
+…
+Step N - …
+
+If the text does not contain a sequence of instructions, \ 
+then simply write \"No steps provided.\"
+
+\"\"\"{text_2}\"\"\"
+"""
+#response = get_completion(prompt)
+#print("Completion for Text:")
+#print(response)
+
+
+####################
+'''
+@Principle 2
+Tactic #2. Instruct the model to work out its own solution before rushing to a conclusion.
+'''
+
+prompt = f"""
+Your task is to determine if the student's solution \
+is correct or not.
+To solve the problem do the following:
+- First, work out your own solution to the problem. 
+- Then compare your solution to the student's solution \ 
+and evaluate if the student's solution is correct or not. 
+Don't decide if the student's solution is correct until 
+you have done the problem yourself.
+
+Use the following format:
+Question:
+```
+question here
+```
+Student's solution:
+```
+student's solution here
+```
+Actual solution:
+```
+steps to work out the solution and your solution here
+```
+Is the student's solution the same as actual solution \
+just calculated:
+```
+yes or no
+```
+Student grade:
+```
+correct or incorrect
+```
+
+Question:
+```
+I'm building a solar power installation and I need help \
+working out the financials. 
+- Land costs $100 / square foot
+- I can buy solar panels for $250 / square foot
+- I negotiated a contract for maintenance that will cost \
+me a flat $100k per year, and an additional $10 / square \
+foot
+What is the total cost for the first year of operations \
+as a function of the number of square feet.
+``` 
+Student's solution:
+```
+Let x be the size of the installation in square feet.
+Costs:
+1. Land cost: 100x
+2. Solar panel cost: 250x
+3. Maintenance cost: 100,000 + 100x
+Total cost: 100x + 250x + 100,000 + 100x = 450x + 100,000
+```
+Actual solution:
+"""
+#esponse = get_completion(prompt)
+#print(response)
